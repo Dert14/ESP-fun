e0dce6e58a731d14bc70d74f052f212c3e67cf6b
Dert14 <marcinn.klimkiewicz@wp.pl>
2022-02-23 20:51:06 +0100

    adafruit add to project

--- OLED_HTD11_main.py
+++ OLED_HTD11_main.py
@@ -2,6 +2,9 @@ from machine import SoftI2C, Pin, Timer
 import dht
 import OLED_HTD11_modules as modules
 import ssd1306
+import ujson as json
+import network, usys
+
 
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
 
@@ -11,8 +14,16 @@ oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
 dht_11 = dht.DHT11(Pin(4))
 
+with open("/wifi_credencials.json") as credentials_json:   # This pattern allows you to open and read an existing file.
+    settings = json.loads(credentials_json.read())
+wlan = network.WLAN(network.STA_IF)
+
+modules.do_connect(wlan, settings)
+
 def work_isr(event):
     modules.get_conditions(dht_11)
+    modules.send_data(settings)
+#   modules.send_hum(settings)
     modules.clear(oled)
     modules.write_text(oled)
 #   modules.dynamic_text(oled)
--- OLED_HTD11_modules.py
+++ OLED_HTD11_modules.py
@@ -1,6 +1,9 @@
 import dht
 import ssd1306
 from utime import sleep_ms
+import network, usys
+import urequests  as requests
+import ujson as json
 
 hum = 0
 temp = 0
@@ -25,6 +28,44 @@ def get_conditions(dht_11):
     hum = dht_11.humidity()
     temp = dht_11.temperature()
 
+def do_connect(wlan, settings):
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(settings["wifi_name"], settings["password"])
+                                                                   
+        if not wlan.isconnected():
+            print("Can't connect to network with given credentials.")
+            usys.exit(0)  # This will programmatically break the execution of this script and return to shell.
+        print('network config:', wlan.ifconfig())
+        
+def send_data(settings):
+    global temp
+    global hum
+    url = 'https://io.adafruit.com/api/v2/' + settings["username"] + '/feeds/' + settings["feed_1"] + '/data'
+    body = {'value': str(temp)+','+str(hum)}
+    headers = {'X-AIO-Key': settings["aio_key"], 'Content-Type': 'application/json'}
+    try:
+        r = requests.post(url, json=body, headers=headers)
+    except Exception as e:
+        print(e)
+        
+    
+'''
+def send_hum(settings):
+    global hum
+    url = 'https://io.adafruit.com/api/v2/' + settings["username"] + '/feeds/' + settings["feed_2"] + '/data'
+    body = {'value': str(hum)}
+    headers = {'X-AIO-Key': settings["aio_key"], 'Content-Type': 'application/json'}
+    try:
+        r = requests.post(url, json=body, headers=headers)
+        print(r.text)
+    except Exception as e:
+        print(e)
+'''
+
+
+
 
 
 '''
@@ -43,6 +84,3 @@ def dynamic_text(display):
 
 
     
-
-    
-
