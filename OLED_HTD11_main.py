1f47ecd901f6b2fb0d99acff5cd9d05bdc5d47e8
Dert14 <marcinn.klimkiewicz@wp.pl>
2022-02-23 11:23:08 +0100

    DHT11 on display

--- OLED_HTD11_main.py
+++ OLED_HTD11_main.py
@@ -1,4 +1,4 @@
-from machine import SoftI2C, Pin
+from machine import SoftI2C, Pin, Timer
 import dht
 import OLED_HTD11_modules as modules
 import ssd1306
@@ -9,7 +9,20 @@ oled_width = 128
 oled_height = 64
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
+dht_11 = dht.DHT11(Pin(4))
 
-dht_11 = dht.DHT11(4)
-while True:
-    modules.static_text(oled)
+def work_isr(event):
+    modules.get_conditions(dht_11)
+    modules.clear(oled)
+    modules.write_text(oled)
+#   modules.dynamic_text(oled)
+    print(dht_11.temperature())
+    oled.show()
+
+    
+blink_timer = Timer(1)
+blink_timer.init(period=1500, mode=Timer.PERIODIC, callback=work_isr)
+
+
+
+
--- OLED_HTD11_modules.py
+++ OLED_HTD11_modules.py
@@ -1,13 +1,48 @@
 import dht
 import ssd1306
+from utime import sleep_ms
 
-def static_text(display):
-    display.text('Temperature:', 0, 0)
-    display.text('Humanity:', 0, 10)
-    display.show()
+hum = 0
+temp = 0
+
+def clear(display):
+    display.fill(0)
+
+def write_text(display):
+    global hum
+    global temp
+    display.text('Temp:'+str(temp), 0, 0)
+    display.text('Hum:'+str(hum), 0, 10)
+#   display.text(str(temp), 35, 0)
+#   display.text(str(hum), 30, 10)
+
+
     
-def dynamic_text()
+def get_conditions(dht_11):
+    global hum
+    global temp
+    dht_11.measure()
+    hum = dht_11.humidity()
+    temp = dht_11.temperature()
 
+
+
+'''
+def dynamic_text(display):
+    hum = 1
+    temp = 2
+    display.text(str(temp), 35, 0)
+    display.text(str(hum), 35, 10)
+    
+'''
+
+    
+    
+
+
+
+
+    
 
     
 
